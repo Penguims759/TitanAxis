7448ac703f378adf58efeaf77dbc6d8eafb16ab7
Penguims759 <69517408+Penguims759@users.noreply.github.com>
2025-07-29 12:55:21 -0300

    Add logging to JpaUtil

--- src/main/java/com/titanaxis/util/JpaUtil.java
+++ src/main/java/com/titanaxis/util/JpaUtil.java
@@ -3,9 +3,15 @@ package com.titanaxis.util;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.slf4j.Logger;
+
+import com.titanaxis.util.AppLogger;
+
+/** Utility class for managing the JPA EntityManagerFactory. */
 
 public class JpaUtil {
 
+    private static final Logger logger = AppLogger.getLogger();
     private static final EntityManagerFactory entityManagerFactory;
 
     static {
@@ -13,7 +19,7 @@ public class JpaUtil {
             // O nome "TitanAxisPU" deve ser o mesmo do persistence.xml
             entityManagerFactory = Persistence.createEntityManagerFactory("TitanAxisPU");
         } catch (Throwable ex) {
-            System.err.println("Falha ao criar o EntityManagerFactory." + ex);
+            logger.error("Falha ao criar o EntityManagerFactory.", ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
